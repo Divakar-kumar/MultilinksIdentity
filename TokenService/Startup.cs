fd45dbe46c12408d5ae6681a8aabec28bde1ad27
Chris Dinh <none@none>
2017-10-24 13:09:39 +1300

    TokenService: Update basic settings in Startup.cs

--- TokenService/Startup.cs
+++ TokenService/Startup.cs
@@ -9,26 +9,25 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace TokenService
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+   public class Startup
+   {
+      // This method gets called by the runtime. Use this method to add services to the container.
+      // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddIdentityServer()
+            .AddDeveloperSigningCredential();
+      }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+      {
+         if(env.IsDevelopment())
+         {
+            app.UseDeveloperExceptionPage();
+         }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
+         app.UseIdentityServer();
+      }
+   }
 }
