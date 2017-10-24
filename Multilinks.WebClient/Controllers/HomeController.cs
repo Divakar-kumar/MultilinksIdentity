3aad1de7d53bfd2b2739912ff651bbbbc5452401
Chris Dinh <none@none>
2017-10-24 21:35:46 +1300

    WebClient: Add logout
    --HG--
    branch : feature/client/web

--- Multilinks.WebClient/Controllers/HomeController.cs
+++ Multilinks.WebClient/Controllers/HomeController.cs
@@ -2,17 +2,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Multilinks.WebClient.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Multilinks.WebClient.Controllers
 {
    public class HomeController : Controller
    {
+      [HttpGet]
       public IActionResult Index()
       {
          return View();
       }
 
       [Authorize]
+      [HttpGet]
       public IActionResult About()
       {
          ViewData["Message"] = "Your application description page.";
@@ -20,11 +23,22 @@ namespace Multilinks.WebClient.Controllers
          return View();
       }
 
+      [HttpGet]
       public IActionResult Contact()
       {
          ViewData["Message"] = "Your contact page.";
 
          return View();
+      }
+
+      [Authorize]
+      [HttpPost]
+      public IActionResult Logout()
+      {
+         return SignOut(new AuthenticationProperties
+         {
+            RedirectUri = "/Home/Index"
+         }, "Cookies", "oidc");
       }
 
       public IActionResult Error()
