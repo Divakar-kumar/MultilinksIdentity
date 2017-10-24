fdcd1132fc1ce655f2fd9dc0a8dd2cfdce91ba88
Chris Dinh <none@none>
2017-10-24 19:28:50 +1300

    TokenService: project rename

--- Multilinks.TokenService/Program.cs
+++ Multilinks.TokenService/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace TokenService
+namespace Multilinks.TokenService
 {
     public class Program
     {
--- Multilinks.TokenService/Startup.cs
+++ Multilinks.TokenService/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace TokenService
+namespace Multilinks.TokenService
 {
    public class Startup
    {
