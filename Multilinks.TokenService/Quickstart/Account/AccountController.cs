c13ad161f1db2b059157d98ccb5522b33bf11e0a
Chris Dinh <none@none>
2017-10-25 22:45:39 +1300

    TokenService: Removed Cancel button from login page
    --HG--
    branch : feature/tokenservice

--- Multilinks.TokenService/Quickstart/Account/AccountController.cs
+++ Multilinks.TokenService/Quickstart/Account/AccountController.cs
@@ -74,27 +74,6 @@ namespace IdentityServer4.Quickstart.UI
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginInputModel model, string button)
         {
-            if (button != "login")
-            {
-                // the user clicked the "cancel" button
-                var context = await _interaction.GetAuthorizationContextAsync(model.ReturnUrl);
-                if (context != null)
-                {
-                    // if the user cancels, send a result back into IdentityServer as if they 
-                    // denied the consent (even if this client does not require consent).
-                    // this will send back an access denied OIDC error response to the client.
-                    await _interaction.GrantConsentAsync(context, ConsentResponse.Denied);
-                    
-                    // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
-                    return Redirect(model.ReturnUrl);
-                }
-                else
-                {
-                    // since we don't have a valid context, then we just go back to the home page
-                    return Redirect("~/");
-                }
-            }
-
             if (ModelState.IsValid)
             {
                 // validate username/password against in-memory store
