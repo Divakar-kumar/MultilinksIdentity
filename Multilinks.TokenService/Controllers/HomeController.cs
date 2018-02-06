291d7c86271ffabcbfb88e0c68c16a06d3afa073
Chris Dinh <none@none>
2018-02-06 21:16:05 +1300

    TokenService: Remove About & Contact view
    --HG--
    branch : feature/tokenservice

--- Multilinks.TokenService/Controllers/HomeController.cs
+++ Multilinks.TokenService/Controllers/HomeController.cs
@@ -15,20 +15,6 @@ namespace Multilinks.TokenService.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel());
