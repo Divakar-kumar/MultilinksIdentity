f81465ff10801c3b8e2bdbd6a7c9294031438a6a
Chris Dinh <none@none>
2018-02-20 20:33:08 +1300

    SpaClient: Add SpaClient from VS Angular template
    --HG--
    branch : feature/spaclient

---
+++ Multilinks.SpaClient/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Multilinks.SpaClient
+{
+   public class Startup
+   {
+      private IConfiguration _configuration { get; }
+      private IHostingEnvironment _env { get; }
+
+      public Startup(IConfiguration configuration, IHostingEnvironment env)
+      {
+         _configuration = configuration;
+         _env = env;
+      }
+
+      public IConfiguration Configuration { get; }
+
+      // This method gets called by the runtime. Use this method to add services to the container.
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddMvcCore()
+            .AddRazorViewEngine()
+            .AddMvcOptions(opt =>
+            {
+               if(!_env.IsProduction())
+               {
+                  var launchJsonConfig = new ConfigurationBuilder()
+                        .SetBasePath(_env.ContentRootPath)
+                        .AddJsonFile("Properties\\launchSettings.json")
+                        .Build();
+                  opt.SslPort = launchJsonConfig.GetValue<int>("iisSettings:iisExpress:sslPort");
+               }
+               opt.Filters.Add(new RequireHttpsAttribute());
+            });
+
+         services.AddRouting(opt => opt.LowercaseUrls = true);
+      }
+
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app)
+      {
+         if(_env.IsDevelopment())
+         {
+            app.UseDeveloperExceptionPage();
+            app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+            {
+               HotModuleReplacement = true
+            });
+         }
+         else
+         {
+            app.UseExceptionHandler("/Home/Error");
+         }
+
+         app.UseStaticFiles();
+
+         app.UseMvc(routes =>
+         {
+            routes.MapRoute(
+                   name: "default",
+                   template: "{controller=Home}/{action=Index}/{id?}");
+
+            routes.MapSpaFallbackRoute(
+                   name: "spa-fallback",
+                   defaults: new { controller = "Home", action = "Index" });
+         });
+      }
+   }
+}
