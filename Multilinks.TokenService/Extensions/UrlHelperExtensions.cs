8b1d8000f698f33d67922dade23ed0d8cf6f7be3
Chris Dinh <dinh.thanhtoan@gmail.com>
2018-06-19 22:56:14 +1200

    TS-1800045: Handle registration confirmation (i.e. update user details)

--- Multilinks.TokenService/Extensions/UrlHelperExtensions.cs
+++ Multilinks.TokenService/Extensions/UrlHelperExtensions.cs
@@ -8,10 +8,10 @@ namespace Microsoft.AspNetCore.Mvc
 {
     public static class UrlHelperExtensions
     {
-        public static string EmailConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        public static string RegisterConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
         {
             return urlHelper.Action(
-                action: nameof(AccountController.ConfirmEmail),
+                action: nameof(AccountController.RegisterConfirmation),
                 controller: "Account",
                 values: new { userId, code },
                 protocol: scheme);
