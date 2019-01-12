81be6daf7a41b8c781b84735067594329776b154
Chris Dinh <dinh.thanhtoan@gmail.com>
2019-01-12 22:17:49 +1300

    work-id-135: Update HubConnectionEntity schema

--- Multilinks.ApiService/Entities/HubConnectionEntity.cs
+++ Multilinks.ApiService/Entities/HubConnectionEntity.cs
@@ -4,12 +4,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Multilinks.ApiService.Entities
 {
-    public class HubConnectionEntity
-    {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Key]
-        public long Id { get; set; }
-        public string ConnectionID { get; set; }
-        public Guid EndpointId { get; set; }
-    }
+   public class HubConnectionEntity
+   {
+      [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+      [Key]
+      public long Id { get; set; }
+      public string ConnectionID { get; set; }
+      public Guid EndpointId { get; set; }
+      public string AccessLevel { get; set; }
+      public string Token { get; set; }
+   }
 }
