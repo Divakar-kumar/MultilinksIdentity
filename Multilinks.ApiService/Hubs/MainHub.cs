3838b160b093e761270587a22e8bd61beb659408
Chris Dinh <dinh.thanhtoan@gmail.com>
2019-01-20 21:46:59 +1300

    work-id-145: Associate hub connection Id to endpoint Id

--- Multilinks.ApiService/Hubs/MainHub.cs
+++ Multilinks.ApiService/Hubs/MainHub.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Multilinks.ApiService.Services;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +11,28 @@ namespace Multilinks.ApiService.Hubs
    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
    public class MainHub : Hub
    {
+      private readonly IHubConnectionService _hubConnectionService;
+
+      public MainHub(IHubConnectionService hubConnectionService)
+      {
+         _hubConnectionService = hubConnectionService;
+      }
+
       public override async Task OnConnectedAsync()
       {
          Context.Items["Role"] = Context.User.Claims.FirstOrDefault(c => c.Type == "role")?.Value;
          Context.Items["EndpointId"] = Context.GetHttpContext().Request.Query["ep"];
+
+         var connectionReferenceUpdated = await _hubConnectionService.UpdateHubConnectionReferenceAsync(
+            Guid.Parse(Context.Items["EndpointId"].ToString()),
+            Context.ConnectionId,
+            Context.ConnectionAborted);
+
+         if(!connectionReferenceUpdated)
+         {
+            Context.Abort();
+         }
+
          await base.OnConnectedAsync();
       }
    }
