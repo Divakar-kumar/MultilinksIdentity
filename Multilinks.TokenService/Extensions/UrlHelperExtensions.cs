d6288a905b9cf27b6b4cb0cf2274523df8fbc35d
Chris Dinh <dinh.thanhtoan@gmail.com>
2019-05-08 22:51:44 +1200

    work-id-187: Remove ResetPassword actions and view (Attempt 2)

--- Multilinks.TokenService/Extensions/UrlHelperExtensions.cs
+++ Multilinks.TokenService/Extensions/UrlHelperExtensions.cs
@@ -1,29 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Multilinks.TokenService.Controllers;
 
 namespace Microsoft.AspNetCore.Mvc
 {
-    public static class UrlHelperExtensions
-    {
-        public static string RegisterConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
-        {
-            return urlHelper.Action(
-                action: nameof(AccountController.RegisterConfirmation),
-                controller: "Account",
-                values: new { userId, code },
-                protocol: scheme);
-        }
-
-        public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
-        {
-            return urlHelper.Action(
-                action: nameof(AccountController.ResetPassword),
-                controller: "Account",
-                values: new { userId, code },
-                protocol: scheme);
-        }
-    }
+   public static class UrlHelperExtensions
+   {
+      public static string RegisterConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+      {
+         return urlHelper.Action(
+            action: nameof(AccountController.RegisterConfirmation),
+            controller: "Account",
+            values: new { userId, code },
+            protocol: scheme);
+      }
+   }
 }
