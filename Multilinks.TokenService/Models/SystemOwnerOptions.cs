58a5a6cffee1f26db4b3a653cd02618fe18b7493
Chris Dinh <dinh.thanhtoan@gmail.com>
2019-07-31 23:24:23 +1200

    Add system owner poco

---
+++ Multilinks.TokenService/Models/SystemOwnerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Multilinks.TokenService.Models
+{
+   public class SystemOwnerOptions
+   {
+      public string Email { get; set; }
+
+      public string FirstName { get; set; }
+
+      public string LastName { get; set; }
+
+      public string DefaultPassword { get; set; }
+   }
+}
